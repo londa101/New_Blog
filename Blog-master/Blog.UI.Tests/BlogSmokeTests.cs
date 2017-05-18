93f2f7058076a212544915e2ac2fa1f9c9e13323
londa101 <londa101@abv.bg>
2017-05-18 16:02:41 +0300

    add

---
+++ Blog-master/Blog.UI.Tests/BlogSmokeTests.cs
@@ -0,0 +1,29 @@
+﻿using Blog.UI.Tests.Pages.HomePage;
+using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.UI.Tests
+{
+    [TestFixture]
+    public class BlogSmokeTests
+    {
+        [Test]
+        public void BlogLogoDisplayedRightMessage()
+        {
+            var homePage = new HomePage(BrowserHost.Instance.Application.Browser);
+
+            homePage.NavigateTo();
+
+            homePage.AssertLogo();
+        }
+
+
+    }
+}

6eb41c8967e5a9ead6143d4635e43f21ed9b6ffd
londa101 <londa101@abv.bg>
2017-05-22 22:58:27 +0300

    add ArticlePage
    add basepage and folder ArticlePage

---
+++ Blog-master/Blog/Pages/Article/ArticlePage.cs
@@ -0,0 +1,11 @@
+﻿using OpenQA.Selenium;
+
+namespace Blog.Pages.Article
+{
+    public class ArticlePage : BasePage
+    {
+        public ArticlePage(IWebDriver driver) : base(driver)
+        {
+        }
+    }
+}
---
+++ Blog-master/Blog/Pages/BasePage.cs
@@ -0,0 +1,35 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace Blog.Pages
+{
+    public class BasePage
+    {
+       
+            private IWebDriver driver;
+            private WebDriverWait wait;
+
+            public BasePage(IWebDriver driver)
+            {
+                this.driver = driver;
+                this.wait = new WebDriverWait(this.driver, TimeSpan.FromSeconds(10));
+            }
+
+            public IWebDriver Driver
+            {
+                get
+                {
+                    return this.driver;
+                }
+            }
+
+            public WebDriverWait Wait
+            {
+                get
+                {
+                    return this.wait;
+                }
+            }
+        }   
+}
